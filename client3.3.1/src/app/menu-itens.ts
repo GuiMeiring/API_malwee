4e0187b91ef877da45342ced5e2235f8f4151c2e
guilherme meiring <guilhermemeiring08@gmail.com>
2022-10-27 21:50:44 -0300

    Corregido

---
+++ client3.3.1/src/app/menu-itens.ts
@@ -0,0 +1,32 @@
+import { CollectionComponent } from "./collection/collection.component";
+import { GroupComponent } from "./group/group.component";
+import { SubgroupComponent } from "./subgroup/subgroup.component";
+import { UserComponent } from "./user/user.component";
+
+export const MenuItens = [
+    {
+        path: 'group',
+        caption : 'Grupo',
+        icon : 'assessment',
+        component: GroupComponent,
+    },
+    {
+        path: 'subgroup',
+        caption : 'Sub Grupo',
+        icon : 'class',
+        component: SubgroupComponent,
+    },
+    {
+        path: 'collection',
+        caption : 'Coleções',
+        icon : 'horizontal_split',
+        component: CollectionComponent,
+    },
+    {
+        path: 'user',
+        caption : 'Usuário',
+        icon : 'person',
+        component: UserComponent,
+    },
+    
+]
